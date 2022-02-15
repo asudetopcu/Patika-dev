d3912c4e1b6cab1b573ec96c2d672ded81e52534
Asude Topcu <96980341+asudetopcu@users.noreply.github.com>
2022-02-15 16:05:48 +0300

    Add files via upload

--- hypotenuseCalculation.java
+++ hypotenuseCalculation.java
@@ -1,8 +1,19 @@
-
+import java.util.Scanner;
 public class hypotenuseCalculation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Üçgen için dik kenar uzunluklarýný giriniz. ");
+		Scanner input=new Scanner(System.in);
+		double kenar1=input.nextDouble();
+		double kenar2=input.nextDouble();
+		
+		double kenar3 = Math.sqrt((kenar1*kenar1)+(kenar2*kenar2));
+		
+		double u=(kenar1+kenar2+kenar3)/2;
+		
+		double ucgeninAlaný=u*(u-kenar1)*(u-kenar2)*(u-kenar3);
+		
+		System.out.println("Üçgenin alaný= " +ucgeninAlaný);
 
 	}
 
