7b3674111cbb941890add3650ae718fa17588e94
Asude Topcu <96980341+asudetopcu@users.noreply.github.com>
2022-02-21 10:43:43 +0300

    Add files via upload

---
+++ dongulerKuvvet.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class dongulerKuvvet {
+
+	public static void main(String[] args) {
+		
+		int n;
+		Scanner input =new Scanner(System.in);
+		System.out.println("Sýnýr sayýsý giriniz: ");
+	    n = input.nextInt();
+	    
+	    System.out.println("4'ÜN KATLARI");
+	    for(int i=1; i<=n; i*=4) {
+	    	System.out.println(i + ",");
+	    }
+	    System.out.println("5'ÝN KATLARI");
+		for (int i=1; i<=n; i*=5) {
+			System.out.println(i+ ",");
+		}
+		
+
+	}
+
+}
